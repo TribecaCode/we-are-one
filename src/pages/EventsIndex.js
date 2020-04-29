a62f312c730a9d0faa99cbc4fcedea282e0f3752
andrei <andrew.motinga@gmail.com>
2020-04-29 15:40:58 -0700

    asdf

--- src/pages/EventsIndex.js
+++ src/pages/EventsIndex.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Event from './Event'
 
-const EventsIndex = () => {
+const EventsIndex = ({ items }) => {
   return (
-    <div>
-      <h1> events index page</h1>
-    </div>
+<div class="bg-wrapper bg-festival">
+	<div class="container-fluid">
+		<div class="container container--content">
+      <div class="row">
+        {items.map(item => <Event key={item.id} item={item}/>)}
+      </div>
+		</div>
+	</div>
+</div>
   )
 }
 
