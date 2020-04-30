8ebd02c2be10bc0d67db5c77224a98fa0f28302e
andrei <andrew.motinga@gmail.com>
2020-04-30 11:53:18 -0700

    fix fonts

--- src/pages/EventsIndex.js
+++ src/pages/EventsIndex.js
@@ -3,10 +3,10 @@ import Event from './Event'
 
 const EventsIndex = ({ items }) => {
   return (
-<div class="bg-wrapper bg-festival">
-	<div class="container-fluid">
-		<div class="container container--content">
-      <div class="row">
+<div className="bg-wrapper bg-festival">
+	<div className="container-fluid">
+		<div className="container container--content">
+      <div className="row">
         {items.map(item => <Event key={item.id} item={item}/>)}
       </div>
 		</div>
